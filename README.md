cc2e2a09d57993d3491a71caad7fb146d774c00f
TakahashiIkki <new_ittyandesu@outlook.jp>
2017-03-04 15:40:51 +0900

    It is First Commit

---
+++ README.md
@@ -0,0 +1,56 @@
+# VaporTrySwiftHackSQLite
+
+# Project
+This is sample for Server-Side-Swift.
+I tried "Vapor" (this is Server Side Swift framework).
+
+I tried `CRUD` using Vapor by SQLite.
+
+And now, I can `Create` and `Read`.
+
+# How to Check
+
+Please Set up Vapor.
+[How to Setup Vapor](https://vapor.github.io/documentation/)
+
+And so, Please `git clone` this project.
+
+So After,
+
+```
+$ vapor build
+$ vapor run serve
+``` 
+
+Then, Enter 
+- GET `http://localhost:8080/users`
+-- You can get all of users table data.
+
+
+- GET `http://localhost:8080/users/:id`
+-- You can get one of users table data by id.
+
+
+- POST `http://localhost:8080/uses`  
+-- You can save request data.
+
+They are sample for features of routing by "Vapor".
+
+# CopyLight
+@2017 高橋　一騎 All Rights Reserved.
+
+# Basic Template
+
+A basic vapor template for starting a new Vapor web application. If you're using vapor toolbox, you can use: `vapor new --template=basic`
+
+## 📖 Documentation
+
+Visit the Vapor web framework's [documentation](http://docs.vapor.codes) for instructions on how to use this package.
+
+## 💧 Community
+
+Join the welcoming community of fellow Vapor developers in [slack](http://vapor.team).
+
+## 🔧 Compatibility
+
+This package has been tested on macOS and Ubuntu.
---
+++ Sources/App/main.swift
@@ -0,0 +1,69 @@
+import Vapor
+import VaporSQLite
+
+let drop = Droplet()
+try drop.addProvider(VaporSQLite.Provider.self)
+
+// This is Defaul
+drop.get { req in
+    return try drop.view.make("welcome", [
+    	"message": drop.localization[req.lang, "welcome", "title"]
+    ])
+}
+
+// This is Defaul
+drop.resource("posts", PostController())
+
+/*
+ * GET 
+ * version
+ * You can get `sqlite_version`
+ */
+drop.get("version") { request in
+    let result = try drop.database?.driver.raw("SELECT sqlite_version()")
+    return try JSON(node: result)
+}
+
+/*
+ * GET
+ * users
+ * You can get all of `users`(the table on sqlite) data.
+ */
+drop.get("users") { request in
+    let result = try drop.database?.driver.raw("SELECT email, password FROM users")
+    
+    return try JSON(node: result)
+}
+
+/*
+ * GET
+ * users/:id
+ * You can get `users`(the table on sqlite) data By id(request).
+ */
+drop.get("users", Int.self) { request, userId in
+    let result = try drop.database?.driver.raw("SELECT email, password FROM users WHERE id = ?", [userId])
+    
+    return try JSON(node: result)
+}
+
+/*
+ * POST
+ * users
+ * You can create `users`(the table on sqlite) data By request.
+ * parameter:  {"email":"xxx","password":"xxx"}
+ */
+drop.post("users") { request in
+    guard let email = request.json?["email"]?.string! else {
+        throw Abort.custom(status: .badRequest, message: "Sorry, Email is Missing")
+    }
+    
+    guard let password = request.json?["password"]?.string! else {
+        throw Abort.custom(status: .badRequest, message: "Sorry, Password is Missing")
+    }
+    
+    let result = try drop.database?.driver.raw("INSERT INTO users(email, password) VALUES(?, ?)", [email, password])
+    
+    return try JSON(node: result)
+}
+
+drop.run()
